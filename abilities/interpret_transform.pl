dbe783a7c5e7765314d77c56f3b17b5e77ee7dd0
Nicholas Paun <nicholas.paun@mail.mcgill.ca>
2018-06-16 18:06:12 -0400

    Deal with applystat and opponent_context in a confusing sort of way

--- abilities/interpret_transform.pl
+++ abilities/interpret_transform.pl
@@ -1,6 +1,44 @@
+relates_to_opponent(draw(opponent,_)).
+relates_to_opponent(shuffle(opponent)).
+relates_to_opponent(deck(opponent,_,_,_)).
 
-merge_applystat(A,T,[A|T]).
-merge_applystat(applystat(Status,Target),[applystat(StatusS,Target)|ActionS],[applystat([Status|StatusS],Target)|ActionS]).
+
+opponent_context_(Action, 
+  (Buf,[Action|OpponentActionS],ActionS), 
+  (Buf, OpponentActionS, ActionS)
+) :-
+  relates_to_opponent(Action).
+
+opponent_context_(Action,(Buf,[],BufRes), (opponent_context([Action|OpponentActionS]), OpponentActionS, ActionS)) :-
+  (Buf = nil -> BufRes = ActionS; BufRes = [Buf|ActionS]),
+  relates_to_opponent(Action).
+
+opponent_context_(Action,(Buf,[],BufRes), (Action, [], ActionS)) :-
+  (Buf = nil -> BufRes = ActionS; BufRes = [Buf|ActionS]).
+
+opponent_context(ActionS, Contextualized) :-
+  foldl(opponent_context_,ActionS,(nil,[],Contextualized), (LastAction,[],[LastAction])).
+
+
+
+%
+
+glue(A,L,[A|L]) :- is_list(L).
+glue(A,V,[A|[V]]).
+
+merge_applystat_(
+      applystatus(Status, Target),
+      (applystatus(StatusS, Target), ActionS), 
+      (applystatus(StatusList, Target), ActionS)
+) :-
+  glue(Status,StatusS,StatusList).
+
+merge_applystat_(Action, (Buf, [Buf|ActionS]), (Action, ActionS)).
+
+merge_applystat([Action|ActionS],Merged) :-
+    foldl(merge_applystat_,ActionS,(Action,Merged),(LastAction,[LastAction])).
+
+
 
 
 
