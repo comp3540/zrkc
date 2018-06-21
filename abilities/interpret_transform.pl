195906c65dd2cb480df89d360fc3772647610640
Rose Dufresne <rose.dufresne@outlook.com>
2018-06-21 18:36:08 -0400

    interpret_transform now finished: Some code was simplified using "foldr" and we now have a simple traversal function for the ability AST

--- abilities/interpret_transform.pl
+++ abilities/interpret_transform.pl
@@ -1,23 +1,57 @@
+:- module(interpret_transform,[prepare_ability/2]).
+
+prepare_ability(ability(Name,ActionS),ability(Name,TActionS)) :-
+	traversal(ActionS,TActionS).
+
+transform(Action0,ActionF) :-
+	rle(flip_run,Action0,Action1),
+	merge_applystat(Action1,Action2),
+	opponent_context(Action2,ActionF).
+
+traversal_action(add(Who,Trigger,ActionS), add(Who,Trigger,TActionS)) :-
+	traversal(ActionS,TActionS), !.
+
+traversal_action(cond(If,then(True),else(False)), cond(If,then(TTrue),else(TFalse))) :-
+	traversal(True,TTrue),
+	traversal(False,TFalse), !.
+
+traversal_action(Action,Action).
+
+traversal_list([],[]).
+
+traversal_list([Action|ActionS],[TransformedAction|TransformedActionS]) :-
+	traversal_action(Action,TransformedAction),
+	traversal_list(ActionS,TransformedActionS).
+
+traversal(null,null) :- !.
+
+traversal(L, Res) :-
+	transform(L,Transformed),
+	traversal_list(Transformed,Res).
+
+
+foldr(Reduction, ListI, ListO) :-
+	reverse(ListI,[Last|Rest]),
+	foldl(Reduction, Rest, [Last|[]], ListO).
+
+opponent_context(ActionS,Result) :-
+	foldr(opponent_context_, ActionS, Result).
+
 relates_to_opponent(draw(opponent,_)).
 relates_to_opponent(shuffle(opponent)).
 relates_to_opponent(deck(opponent,_,_,_)).
 
+opponent_context_(OpponentAction, AccI, AccO) :- 
+	relates_to_opponent(OpponentAction),
+	AccI = [opponent_context(OpponentActionS)|OtherActionS],
+	AccO = [opponent_context([OpponentAction|OpponentActionS])|OtherActionS].
 
-opponent_context_(Action, 
-  (Buf,[Action|OpponentActionS],ActionS), 
-  (Buf, OpponentActionS, ActionS)
-) :-
-  relates_to_opponent(Action).
+opponent_context_(OpponentAction, AccI, AccO) :-
+	relates_to_opponent(OpponentAction),
+	AccO = [opponent_context([OpponentAction])|AccI].
 
-opponent_context_(Action,(Buf,[],BufRes), (opponent_context([Action|OpponentActionS]), OpponentActionS, ActionS)) :-
-  (Buf = nil -> BufRes = ActionS; BufRes = [Buf|ActionS]),
-  relates_to_opponent(Action).
-
-opponent_context_(Action,(Buf,[],BufRes), (Action, [], ActionS)) :-
-  (Buf = nil -> BufRes = ActionS; BufRes = [Buf|ActionS]).
-
-opponent_context(ActionS, Contextualized) :-
-  foldl(opponent_context_,ActionS,(nil,[],Contextualized), (LastAction,[],[LastAction])).
+opponent_context_(Action, AccI, AccO) :-
+	AccO = [Action|AccI].
 
 
 
@@ -26,17 +60,23 @@ opponent_context(ActionS, Contextualized) :-
 glue(A,L,[A|L]) :- is_list(L).
 glue(A,V,[A|[V]]).
 
-merge_applystat_(
-      applystatus(Status, Target),
-      (applystatus(StatusS, Target), ActionS), 
-      (applystatus(StatusList, Target), ActionS)
-) :-
-  glue(Status,StatusS,StatusList).
+merge_applystat_(See, Args, Result) :- 
+  	See = applystatus(Status,Target),
+	Args = (applystatus(StatusS,Target), ActionS),
+	glue(Status,StatusS,StatusList),
+	Result = (applystatus(StatusList,Target), ActionS),
+	!.
 
-merge_applystat_(Action, (Buf, [Buf|ActionS]), (Action, ActionS)).
+merge_applystat_(Action, Args, Result) :-
+	% It's not an applystat
+	Args = (BufAction, [BufAction|ActionS]),
+	Result = (Action, ActionS),
+	!.
 
-merge_applystat([Action|ActionS],Merged) :-
-    foldl(merge_applystat_,ActionS,(Action,Merged),(LastAction,[LastAction])).
+merge_applystat([Action|ActionS],Head) :-
+	Initial = (Action,Head),
+    	foldl(merge_applystat_,ActionS,Initial,Result),
+	Result = (LastAction,[LastAction|[]]).
 
 
 
@@ -54,12 +94,29 @@ flip_run(N,cond(if(flip),True,False),cond(if(flip(N),True,False))).
  * indicate that RLE should not be be performed for value X.
  */
 
-rle_(X, (Pattern,Run0,RunS), (Pattern,Run1,RunS)) :- 
-  call(Pattern, N0, X, Run0), 
+% Incrementing count > 2
+rle_(X, Args, Result) :-
+  Args = (Pattern, XN0, RunS), 
+  call(Pattern, N0, X, XN0), 
   succ(N0,N1), 
-  call(Pattern, N1, X, Run1), !.
+  call(Pattern, N1, X, XN1),
+  Result = (Pattern, XN1, RunS), !.
 
-rle_(X, (Pattern,X,RunS), (Pattern,X2,RunS)) :- call(Pattern, 2, X, X2), !.
-rle_(V, (Pattern,Run,[Run|RunS]), (Pattern,V,RunS)) :- V \= Run, !.
+% 2 of the same
+rle_(X, Args, Result) :- 
+	Args = (Pattern, X, RunS),
+	call(Pattern, 2, X, X2), 
+	Result = (Pattern, X2, RunS), !.
 
-rle(Pattern,[V|VS],Encoded) :- foldl(rle_,VS,(Pattern,V,Encoded),(Pattern,LastRun,[LastRun])).
+% When I see some value V, and it doesn't equal the Run value, then I copy Run onto the answers,
+% because I know that Run is finished. Any other runs will use the same Pattern function, because we don't change it
+% Any other runs have to start with V, and they'll have to be written in the slot after Run, which we call RunS. 
+rle_(V, Args, Result) :- 
+	Args = (Pattern, Run, [Run|RunS]),
+	Result = (Pattern, V, RunS),
+	!.
+
+rle(Pattern,[V|VS],Head) :- 
+	Initial = (Pattern, V, Head),
+	foldl(rle_,VS,Initial,Final),
+	Final = (Pattern, LastRun, [LastRun|[]]).
